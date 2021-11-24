cc8961be7985c8d0e76e626e733a16cbd80bd545
Reiya <reiya0104@ezweb.ne.jp>
2021-11-24 16:51:05 +0900

    Update vite.config and upgrade vite version

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
-module.exports = {
-  base: '/vite-vue-router-tailwind-test',
-  publicDir: '/vite-vue-router-tailwind-test',
-  assetsDir: './',
-  outDir: 'docs',
-}
+import path from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // 本番時はgithubリポジトリをルートパスにする
+  base: process.env.NODE_ENV === 'production' ? '/vite-vue-router-tailwind-test/' : './',
+  // resolve: {
+  //   // @/ によるimportをできるようにする。
+  //   alias: {
+  //     '@': path.resolve('__dirname', '/src')
+  //   }
+  // },
+  build: {
+    outDir: 'docs'
+  },
+  plugins: [vue()]
+});
