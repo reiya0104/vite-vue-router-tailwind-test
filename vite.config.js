663ae694e122f651d718950e0c6507fa3f1faff2
Reiya <reiya0104@ezweb.ne.jp>
2021-11-24 17:21:30 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   // 本番時はgithubリポジトリをルートパスにする
-  base: process.env.NODE_ENV === 'production' ? '/vite-vue-router-tailwind-test/' : './',
-  // resolve: {
-  //   // @/ によるimportをできるようにする。
-  //   alias: {
-  //     '@': path.resolve('__dirname', '/src')
-  //   }
-  // },
+  base: (process.env.NODE_ENV === 'production')
+    ? '/vite-vue-router-tailwind-test/' : './',
   build: {
     outDir: 'docs'
   },
